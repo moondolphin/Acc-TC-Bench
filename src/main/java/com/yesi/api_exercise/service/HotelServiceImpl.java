bebc461c1ea127864e378c74c226b1aeaa812753
CamilaDuarte7 <kmi.mica12@gmail.com>
2025-09-09 16:09:49 -0300

    RFU0002: Hotels by date and place.

--- src/main/java/com/yesi/api_exercise/service/HotelServiceImpl.java
+++ src/main/java/com/yesi/api_exercise/service/HotelServiceImpl.java
@@ -7,12 +7,16 @@ import com.yesi.api_exercise.repository.HotelRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 // En HotelServiceImpl.java
 @Service
 @RequiredArgsConstructor
 public class HotelServiceImpl implements HotelService {
+
+    private final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     private final HotelRepository hotelRepository;
     private final HotelMapper hotelMapper;
@@ -23,5 +27,16 @@ public class HotelServiceImpl implements HotelService {
         return hotels.stream()
                      .map(hotelMapper::toResponseDTO)
                      .collect(Collectors.toList());
+    }
+
+   @Override
+    public List<HotelResponseDTO> findAllHotelsByDateAndPlace(String availableFrom, String availableTo, String place){
+        List<Hotel> hotels = hotelRepository.findAll();
+        return hotels.stream().filter(hotel -> 
+            hotel.getAvailableFrom().isBefore(LocalDate.parse(availableFrom, dateFormatter)) &&
+            hotel.getAvailableTo().isAfter(LocalDate.parse(availableTo, dateFormatter)) &&
+            hotel.getPlace().equals(place)).map(hotelMapper::toResponseDTO)
+            .toList();
+    
     }
 }
